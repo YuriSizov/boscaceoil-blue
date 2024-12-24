99d7c66b1587fe5bb254a7e2b3a5120f32cb5705
Yuri Sizov <yuris@humnom.net>
2024-12-24 16:10:41 +0100

    Update the guides to include new features and improve the structure a bit
    Also documents the new Ctrl + LMB action on notes.

--- gui/views/GeneralHelpView.gd
+++ gui/views/GeneralHelpView.gd
@@ -14,6 +14,7 @@ var _shortcut_help: WindowPopup = null
 @onready var _basic_guide_button: SquishyButton = %StartBasicGuide
 @onready var _advanced_guide_button: SquishyButton = %StartAdvancedGuide
 @onready var _show_shortcuts_button: SquishyButton = %ShowShortcutsButton
+@onready var _short_shortcut_list: VBoxContainer = %ShortShortcutList
 
 
 func _init() -> void:
@@ -27,6 +28,10 @@ func _ready() -> void:
 	_basic_guide_button.pressed.connect(Controller.help_manager.start_guide.bind(HelpManager.GuideType.BASIC_GUIDE))
 	_advanced_guide_button.pressed.connect(Controller.help_manager.start_guide.bind(HelpManager.GuideType.ADVANCED_GUIDE))
 	_show_shortcuts_button.pressed.connect(_show_shortcuts)
+	
+	if not Engine.is_editor_hint():
+		Controller.help_manager.reference_node(HelpManager.StepNodeRef.HELP_VIEW, get_global_rect)
+		Controller.help_manager.reference_node(HelpManager.StepNodeRef.HELP_SHORTCUT_SHORTLIST, _short_shortcut_list.get_global_rect)
 
 
 func _notification(what: int) -> void:
