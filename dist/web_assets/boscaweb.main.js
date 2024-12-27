5a8a2a4d2ff23e05c5ce4007ee79425a73bb03af
Yuri Sizov <yuris@humnom.net>
2024-12-27 17:40:05 +0100

    Add checks and guards related to memory allocation in web exports
    This is mainly relevant for Safari which has strict
    budget for each page/worker. This is the main
    reason why we cannot support it. Still, this may be
    fixed by Apple at some point, and with these
    changes we will be ready.
    
    This also adds an early check for memory allocation
    compatibility, so we can even warn the user ahead
    of loading.

--- dist/web_assets/boscaweb.main.js
+++ dist/web_assets/boscaweb.main.js
@@ -9,10 +9,23 @@ const BOSCAWEB_STATE_PROGRESS = 1;
 const BOSCAWEB_STATE_READY    = 2;
 const BOSCAWEB_STATE_FAILED   = 3;
 
+// Can technically be configured with Module["INITIAL_MEMORY"], but we don't have
+// access to that as early as we need it. It seems to be unset though, so using
+// default should be safe.
+const BOSCAWEB_INITIAL_MEMORY = 33554432;
+const BOSCAWEB_MAXIMUM_MEMORY = 2147483648; // 2 GB
+const BOSCAWEB_MEMORY_PAGE_SIZE = 65536;
+
+const BOSCAWEB_COMPATIBILITY_OK = 0;
+const BOSCAWEB_COMPATIBILITY_WARNING = 1;
+const BOSCAWEB_COMPATIBILITY_FAILURE = 2;
+
 class BoscaWeb {
 	constructor() {
 		this.initializing = true;
 		this.engine = new Engine(GODOT_CONFIG);
+		this._allocatedMemory = 0;
+		this.memory = this._allocateWasmMemory();
 		
 		this._bootOverlay = document.getElementById('boot-overlay');
 		this._boot_initialState = document.getElementById('boot-menu');
@@ -34,14 +47,16 @@ class BoscaWeb {
 		
 		this._compat_passedState = document.getElementById('boot-compat-passed');
 		this._compat_failedState = document.getElementById('boot-compat-failed');
+		this._compat_failedHeaderError = document.getElementById('boot-compat-failed-error');
+		this._compat_failedHeaderWarning = document.getElementById('boot-compat-failed-warning');
 		this._compat_failedList = document.getElementById('boot-compat-list');
 		this._compat_tryfixButton = document.getElementById('boot-compat-tryfix');
 		this._compat_tryfixButton.addEventListener('click', () => {
 			this.tryFixCompatibility();
 		});
 		
-		this._compatible = false;
-		this._compatFixable = (GODOT_CONFIG['serviceWorker'] && GODOT_CONFIG['ensureCrossOriginIsolationHeaders'] && 'serviceWorker' in navigator);
+		this._compatLevel = BOSCAWEB_COMPATIBILITY_OK;
+		this._compatFixable = (this.memory && GODOT_CONFIG['serviceWorker'] && GODOT_CONFIG['ensureCrossOriginIsolationHeaders'] && 'serviceWorker' in navigator);
 		
 		// Hidden by default to show native error messages, e.g. if JavaScript
 		// is disabled in the browser.
@@ -49,46 +64,148 @@ class BoscaWeb {
 		this.setState(BOSCAWEB_STATE_INITIAL);
 	}
 	
-	checkCompatibility() {
-		this._bootButton.classList.remove('boot-init-suppressed');
-		this._compat_passedState.style.display = 'none';
-		this._compat_failedState.style.display = 'none';
-		this._compat_tryfixButton.style.display = 'none';
-		this._compat_failedList.style.display = 'none';
-		this._setErrorText(this._compat_failedList, '');
+	_allocateWasmMemory() {
+		// We will try to allocate as much as possible, starting with the limit that we actually require.
+		// In Safari this is likely to fail, so we try less and less. This is not guaranteed to work, but
+		// at least it gives user a chance.
+		const reductionSteps = [ 1, 0.75, 0.5, 0.25 ];
+		let reductionIndex = 0;
 		
+		let wasmMemory = null;
+		let sizeMessage = '';
+		while (wasmMemory == null && reductionIndex < reductionSteps.length) {
+			const reduction = reductionSteps[reductionIndex];
+			this._allocatedMemory = BOSCAWEB_MAXIMUM_MEMORY * reduction;
+			sizeMessage = `${this._humanizeSize(BOSCAWEB_INITIAL_MEMORY)} out of ${this._humanizeSize(this._allocatedMemory)}`;
+			
+			// This can fail if we hit the browser's limit.
+			try {
+				wasmMemory = new WebAssembly.Memory({
+					initial: BOSCAWEB_INITIAL_MEMORY / BOSCAWEB_MEMORY_PAGE_SIZE,
+					maximum: reduction * BOSCAWEB_MAXIMUM_MEMORY / BOSCAWEB_MEMORY_PAGE_SIZE,
+					shared: true
+				});
+			} catch (err) {
+				console.error(err);
+				wasmMemory = null;
+			}
+			
+			reductionIndex += 1;
+		}
+		
+		if (wasmMemory == null) {
+			console.error(`Failed to allocate WebAssembly memory (${sizeMessage}); check the limits.`);
+			return null;
+		}
+		if (!(wasmMemory.buffer instanceof SharedArrayBuffer)) {
+			console.error(`Trying to allocate WebAssembly memory (${sizeMessage}), but returned buffer is not SharedArrayBuffer; this indicates that threading is probably not supported.`);
+			return null;
+		}
+		
+		console.log(`Successfully allocated WebAssembly memory (${sizeMessage}).`);
+		return wasmMemory;
+	}
+	
+	_checkMissingFeatures() {
 		const missingFeatures = Engine.getMissingFeatures({
 			threads: GODOT_THREADS_ENABLED,
 		});
 		
+		return missingFeatures.map((item) => {
+			const itemParts = item.split(' - ');
+			return {
+				'name': itemParts[0],
+				'description': itemParts[1] || '',
+			}
+		});
+	}
+	
+	checkCompatibility() {
+		this._bootButton.classList.remove('boot-init-suppressed');
+		this._compat_passedState.style.display = 'none';
+		this._compat_failedState.style.display = 'none';
+		this._compat_failedHeaderError.style.display = 'none';
+		this._compat_failedHeaderWarning.style.display = 'none';
+		this._compat_tryfixButton.style.display = 'none';
+		this._compat_failedList.style.display = 'none';
+		this._setErrorText(this._compat_failedList, '');
+		
+		this._compatLevel = BOSCAWEB_COMPATIBILITY_OK;
+		
+		// Check memory allocation.
+		if (this.memory == null) {
+			this._lowerCompatibilityLevel(BOSCAWEB_COMPATIBILITY_FAILURE);
+			this._addCompatibilityLevelReason('Your browser does not allow enough memory');
+			
+			const reasonDescription = document.createElement('span');
+			reasonDescription.textContent = `Bosca requested maximum limit of ${this._humanizeSize(BOSCAWEB_MAXIMUM_MEMORY)}, but was refused.`;
+			this._compat_failedList.appendChild(reasonDescription);
+		}
+		else if (this._allocatedMemory < BOSCAWEB_MAXIMUM_MEMORY) {
+			this._lowerCompatibilityLevel(BOSCAWEB_COMPATIBILITY_WARNING);
+			this._addCompatibilityLevelReason('Your browser does not allow enough memory');
+			
+			const reasonDescription = document.createElement('span');
+			reasonDescription.textContent = `Bosca requested maximum limit of ${this._humanizeSize(BOSCAWEB_MAXIMUM_MEMORY)}, but was only allowed ${this._humanizeSize(this._allocatedMemory)}.`;
+			this._compat_failedList.appendChild(reasonDescription);
+		}
+		
+		// Check for missing browser feature.
+		const missingFeatures = this._checkMissingFeatures();
 		if (missingFeatures.length > 0) {
-			this._compatible = false;
-			this._bootButton.classList.add('boot-init-suppressed');
-			this._compat_failedState.style.display = 'flex';
-			this._compat_failedList.style.display = 'block';
-			this._compat_tryfixButton.style.display = (this._compatFixable ? 'inline-block' : 'none');
+			this._lowerCompatibilityLevel(BOSCAWEB_COMPATIBILITY_FAILURE);
+			this._addCompatibilityLevelReason('Your browser is missing following features');
 			
-			const sectionHeader = document.createElement('strong');
-			sectionHeader.textContent = 'Your browser is missing following features: ';
-			this._compat_failedList.appendChild(sectionHeader);
-			
-			const sectionList = document.createElement('span');
-			this._compat_failedList.appendChild(sectionList);
+			const reasonDescription = document.createElement('span');
+			this._compat_failedList.appendChild(reasonDescription);
 			missingFeatures.forEach((item, index) => {
-				const itemParts = item.split(' - ');
-				
 				const annotatedElement = document.createElement('abbr');
-				annotatedElement.textContent = itemParts[0];
-				annotatedElement.title = itemParts[1];
-				sectionList.appendChild(annotatedElement);
+				annotatedElement.textContent = item.name;
+				annotatedElement.title = item.description;
+				reasonDescription.appendChild(annotatedElement);
 				
 				if (index < missingFeatures.length - 1) {
-					sectionList.appendChild(document.createTextNode(", "));
+					reasonDescription.appendChild(document.createTextNode(", "));
 				}
 			});
-		} else {
-			this._compatible = true;
-			this._compat_passedState.style.display = 'flex';
+		}
+		
+		switch (this._compatLevel) {
+			case BOSCAWEB_COMPATIBILITY_OK:
+				this._compat_passedState.style.display = 'flex';
+				break;
+			
+			case BOSCAWEB_COMPATIBILITY_WARNING:
+				this._bootButton.classList.add('boot-init-suppressed');
+				this._compat_failedState.style.display = 'flex';
+				this._compat_failedHeaderWarning.style.display = 'inline-block';
+				this._compat_failedList.style.display = 'block';
+				this._compat_tryfixButton.style.display = (this._compatFixable ? 'inline-block' : 'none');
+				break;
+			
+			case BOSCAWEB_COMPATIBILITY_FAILURE:
+				this._bootButton.classList.add('boot-init-suppressed');
+				this._compat_failedState.style.display = 'flex';
+				this._compat_failedHeaderError.style.display = 'inline-block';
+				this._compat_failedList.style.display = 'block';
+				this._compat_tryfixButton.style.display = (this._compatFixable ? 'inline-block' : 'none');
+				break;
+		}
+	}
+	
+	_addCompatibilityLevelReason(message) {
+		if (this._compat_failedList.hasChildNodes()) {
+			this._compat_failedList.appendChild(document.createElement('br'));
+		}
+		
+		const reasonHeader = document.createElement('strong');
+		reasonHeader.textContent = `${message}: `;
+		this._compat_failedList.appendChild(reasonHeader);
+	}
+	
+	_lowerCompatibilityLevel(level) {
+		if (this._compatLevel < level) {
+			this._compatLevel = level;
 		}
 	}
 	
@@ -204,5 +321,21 @@ class BoscaWeb {
 		
 		this.setState(BOSCAWEB_STATE_FAILED);
 		this.initializing = false;
+	}
+	
+	_humanizeSize(size) {
+		const labels = [ 'B', 'KB', 'MB', 'GB', 'TB', ];
+		
+		let label = labels[0];
+		let value = size;
+		
+		let index = 0;
+		while (value >= 1024 && index < labels.length) {
+			index += 1;
+			value = value / 1024;
+			label = labels[index];
+		}
+		
+		return `${value.toFixed(2)} ${label}`;
 	}
 }
--- dist/web_assets/boscaweb.patches.js
+++ dist/web_assets/boscaweb.patches.js
@@ -51,3 +51,27 @@
 		return forwardedResponse;
 	}
 })(window);
+
+// Monkey-patch the Godot initializer to influence initialization where it cannot be configured.
+(function(window){
+	const _orig_Godot = window.Godot;
+	
+	window.Godot = function(Module) {
+		// Use a pre-allocated buffer that uses a safer amount of maximum memory, which
+		// avoids instant crashes in Safari. Although, there can still be memory issues
+		// in Safari (both macOS and iOS/iPadOS), with some indication of improvements
+		// starting with Safari 18.
+		if (window.bosca.memory != null) {
+			Module["wasmMemory"] = window.bosca.memory;
+		}
+		
+		// The initializer can still throw exceptions, including an out of memory exception.
+		// Due to nested levels of async and promise handling, this is not captured by
+		// try-catching Engine.startGame(). But it can be captured here.
+		try {
+			return _orig_Godot(Module);
+		} catch (err) {
+			window.bosca._fatalError(err);
+		}
+	}
+})(window);
